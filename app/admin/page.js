486d76db91e3e1ecbc7b5e14007b2c50378bbdd7
thirumurugan7 <thiru82003@gmail.com>
2023-05-16 15:59:54 +0530

    working on table data

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,8 +1,13 @@
 "use client";
 import { useEffect, useState } from "react";
 import { AllUsers, UserData } from "@/components/Blockchain";
+import Table from "@/components/Table";
 export default function admin() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState(false);
+  const [gotdata, setGotdata] = useState(false);
+  var thiru = false;
+  const userDataFromChain = [];
+  const userAddressFromChain = [];
   useEffect(() => {
     const getAllUsers = async () => {
       const res = await AllUsers();
@@ -12,18 +17,47 @@ export default function admin() {
       for (let i = 0; i < resLength; i++) {
         console.log(res[i]);
         const address = res[i];
-
+        userAddressFromChain.push(address);
         // const userData = await UserData({ res[0] });
         // console.log(userData);
         console.log(address);
         const userData = await UserData({ address });
-        setUsers((prev) => [...prev, userData]);
-        console.log(userData[0]);
-        console.log(users);
+
+        console.log(userData[0], i);
+
+        userDataFromChain.push(userData);
+        console.log(userDataFromChain);
+        userDataFromChain.map((data) => {
+          console.log(data[0]);
+          console.log(data[4]);
+        });
+        console.log(userAddressFromChain);
       }
+      thiru = true;
+
       //console.log(address);
     };
     getAllUsers();
+    console.log(thiru);
+    if (gotdata) {
+      alert("got it my boy");
+    }
+    console.log(users);
   }, []);
-  return <h1>admin</h1>;
+
+  const table = () => {
+    for (let i = 0; i <= userAddressFromChain.length; i++) {
+      userDataFromChain?.map((data) => (
+        <tr className="border-b dark:border-neutral-500">
+          <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+          <td className="whitespace-nowrap px-6 py-4">{data[0]}</td>
+          <td className="whitespace-nowrap px-6 py-4">{data[1]}</td>
+          <td className="whitespace-nowrap px-6 py-4">{data[2]}</td>
+          <td className="whitespace-nowrap px-6 py-4">{data[2]}</td>
+          <td className="whitespace-nowrap px-6 py-4">{data[2]}</td>
+        </tr>
+      ));
+    }
+  };
+  return thiru && <Table userDataFromChain={userDataFromChain} />;
 }
---
+++ components/Table.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Table = ({ userDataFromChain }) => {
+  console.log(userDataFromChain);
+
+  return (
+    <section className="flex justify-center items-center h-screen pb-[100px]">
+      <div className="overflow-hidden ">
+        <table className="min-w-full text-left text-sm font-light">
+          <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th scope="col" className="px-6 py-4">
+                #
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-4">
+                email
+              </th>
+              <th scope="col" className="px-6 py-4">
+                phone number
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Address
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Status
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {userDataFromChain && alert("we have the data")}
+            {userDataFromChain &&
+              userDataFromChain.map((data) => (
+                <tr className="border-b dark:border-neutral-500">
+                  {console.log(data)}
+                  <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                  <td className="whitespace-nowrap px-6 py-4">{data[0]}</td>
+                  <td className="whitespace-nowrap px-6 py-4">{data[1]}</td>
+                  <td className="whitespace-nowrap px-6 py-4">{data[2]}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default Table;
